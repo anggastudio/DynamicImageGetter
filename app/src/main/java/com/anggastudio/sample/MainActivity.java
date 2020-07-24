a8a183101ab3c6d8bd4964cfe4972326c63e61e3
anggastudio <aa.angga.pratama@gmail.com>
2020-07-24 21:34:38 +0700

    fixed dummy text

--- app/src/main/java/com/anggastudio/sample/MainActivity.java
+++ app/src/main/java/com/anggastudio/sample/MainActivity.java
@@ -2,6 +2,10 @@ package com.anggastudio.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+import com.anggastudio.dynamicimagegetter.DynamicImageGetter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String htmlString = DummyTextHtml.htmlString;
+
+        TextView textView = findViewById(R.id.textView);
+        DynamicImageGetter imageGetter = new DynamicImageGetter(this, textView, DynamicImageGetter.FULL_WIDTH);
+        textView.setText(Html.fromHtml(htmlString, imageGetter, null));
     }
 }
